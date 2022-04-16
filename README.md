c3d573d30845d972c2c2955a26fd72668177b49a
guido2288 <guido2288@gmail.com>
2022-04-16 17:07:53 -0300

    finish!

---
+++ README.md
@@ -0,0 +1,8 @@
+# Faq Accordion Card
+
+Challange from https://www.frontendmentor.io/ 
+
+ FAQ accordion card. 
+
+
+## Thanks for watching 
---
+++ app.js
@@ -0,0 +1,8 @@
+const accordion = document.querySelectorAll('.container');
+
+for (i =0 ; i < accordion.length ; i++) {
+    accordion[i].addEventListener('click' ,  function () {
+        this.classList.toggle('active');
+    } );
+}
+
